71e2e2d9df7292b6430f6f4a49d6e0c6fcfdce81
anbu-chandru <anbuchandru21@gamil.com>
2026-08-20 19:49:19 +0530

    js day-4 task updated

---
+++ JS_tasks/Task-4/assets/js/Task-1.js
@@ -0,0 +1,7 @@
+let age = 25;
+if (18 < age ) {
+    console.log("Eligible")
+}else{
+    console.log("Not Eligible");
+    
+}
---
+++ JS_tasks/Task-4/assets/js/Task-4.js
@@ -0,0 +1,11 @@
+let userName ="admin";
+let password = "1234";
+
+if (userName==="admin" && password === "1234") {
+    console.log("Login Success");
+    
+    
+}else{
+    console.log("Login Invalid");
+    
+}
